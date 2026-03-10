76aafc353b9ab94c9e73fc2309c4ff7c91649ee2
bisswas-kamrul <bisswaskamruldev@gmail.com>
2026-03-10 19:26:08 +0600

    multer updete

--- medelwearFolder/imgUplod.js
+++ medelwearFolder/imgUplod.js
@@ -1,16 +1,15 @@
 const multer  = require('multer')
-
+const path = require('path');
 // ================ Multer storage============================
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "UploaderFolder/")
+    cb(null, "UploaderFolder/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
-    // cb(null, Date.now() + '-' + file.originalname )
-  }
-})
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+  },
+});
 
 const upload = multer({ storage: storage })
 
